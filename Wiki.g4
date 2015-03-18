e447311cf678999a85fa86106d2d0ef1b5881976
Eric Maciel <elm2178@columbia.edu>
2015-03-18 18:05:06 -0400

    added assignment grammar

--- Wiki.g4
+++ Wiki.g4
@@ -1,5 +1,4 @@
-grammar Wiki;
-
+grammar Wiki; 
 /*The Start rule */
 prog: stmt_seq ; 
 
@@ -8,12 +7,39 @@ stmt_seq: stmt NEWLINE stmt_seq
         | /* epsilon */ 
         ;
 
-stmt: print_stmt ;
+stmt: print_stmt 
+    | assignment
+    ;
 
-print_stmt: PRINT '"' ID '"';
+assignment: ID '=' expr;
 
+expr: expr ADD term
+    | expr SUB term
+    | term
+    ;
+
+term: term MUL fact
+    | term DIV fact
+    | fact
+    ;
+
+fact: LPAREN expr RPAREN
+    | number 
+    ;
+
+number: INT;
+
+print_stmt: PRINT STRING;
+
+RPAREN: ')';
+LPAREN: '(';
+MUL: '*'; 
+ADD: '+';
+DIV: '/';
+SUB: '-';
 PRINT: 'print'; 
 ID : [a-zA-Z]+;
 INT: [0-9]+;
+STRING: '"'(~["\\\r\n])+'"';
 NEWLINE: ['\n']+;
 WS : [ \t]+ -> skip;
--- WikiToJava.java
+++ WikiToJava.java
@@ -1,23 +1,38 @@
+import java.io.*;
+import java.util.*;
+
 public class WikiToJava extends WikiBaseListener {
 
+    private LinkedList<String> queue;
     //Translate print to 'System.out.println('
     @Override
     public void enterProg(WikiParser.ProgContext ctx) {
+        queue = new LinkedList<String>();
         System.out.print("public class Test {\n"
-            +"public static void main(String[] args) {"
-            +"System.out.println(\"");
+            +"public static void main(String[] args) {\n");
     }
 
     //Translate } to "
     @Override
     public void exitProg(WikiParser.ProgContext ctx) {
-        System.out.print("\"); } } ");
+        while(queue.peekFirst() != null)
+        {
+            System.out.println(queue.pop());
+        }
+        System.out.print("}\n}");
     }
 
-    //translate integers into 4 digit hex prefixed with \\u
     @Override
     public void enterPrint_stmt(WikiParser.Print_stmtContext ctx) {
-        String word = ctx.ID().getText();
+        System.out.print("System.out.println(\"");
+        String word = ctx.STRING().getText();
+        word = word.substring(1, word.length() - 1);
         System.out.print(word);
     }
+    
+    @Override
+    public void exitPrint_stmt(WikiParser.Print_stmtContext ctx) {
+        System.out.print("\");\n");
+    }
+
 }
