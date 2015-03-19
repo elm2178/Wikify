9c95962aaf8ddc61f2ea28e83f41d7a0fabc53c7
Eric Maciel <elm2178@columbia.edu>
2015-03-19 17:12:49 -0400

    allows you to test with file.wiki and makefile

---
+++ Makefile
@@ -0,0 +1,2 @@
+run: 
+	cat file.wiki | java Test > TestWiki.java && javac TestWiki.java && java TestWiki
--- wikiToJava.java
+++ wikiToJava.java
@@ -5,7 +5,8 @@ public class wikiToJava extends WikiBaseListener {
     //Translate print to 'System.out.println('
     @Override
     public void enterProg(WikiParser.ProgContext ctx) {
-        System.out.print("public class Test {\n" +"public static void main(String[] args) {\n");
+        System.out.print("public class TestWiki {\n" 
+                +"public static void main(String[] args) {\n");
     }
 
     @Override
@@ -15,19 +16,38 @@ public class wikiToJava extends WikiBaseListener {
 
     @Override
     public void enterPrintStmt(WikiParser.PrintStmtContext ctx) {
-        System.out.print("System.out.print(\"");
     }
 
     @Override
     public void exitPrintStmt(WikiParser.PrintStmtContext ctx) {
-        System.out.print("\");\n");
     }
 
+    @Override
+    public void enterAssign(WikiParser.AssignContext ctx) {
+        System.out.println("int " 
+                + ctx.ID().getText() 
+                + " = " 
+                + ctx.expr().getText() 
+                + ";"); 
+    }
+    
+
     public void enterPrintString(WikiParser.PrintStringContext ctx) {
+        System.out.print("System.out.print(\"");
         String word = ctx.STRING().getText();
         word = word.substring(1, word.length() - 1);
         System.out.print(word);
+        System.out.print("\");\n");
     }
+
+    public void enterPrintId(WikiParser.PrintIdContext ctx) {
+        System.out.print("System.out.print(");
+        String id = ctx.ID().getText();
+        System.out.print(id);
+        System.out.print(");\n");
+    }
+
+
 
 
     //Note I wrote this because java's replaceAll would not recognize \\n
