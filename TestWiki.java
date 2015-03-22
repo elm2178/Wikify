92d57991fa4821c5beb811d70a2f377f5dcc806a
lcg2132 <lcg2132@columbia.edu>
2015-03-22 14:09:00 -0400

    just another push

---
+++ TestWiki.java
@@ -0,0 +1,12 @@
+public class TestWiki { 
+public static void main(String[] args) {
+int a = 15;
+int b = 10;
+if (a>b) {
+System.out.print("a is larger than b");
+}
+else {
+}
+System.out.print("\nhi\n");
+}
+}
