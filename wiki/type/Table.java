be734f78b0c4ea903d9ebb6c12a5f6ce23f52d34
sc3399 <sc3399@columbia.edu>
2015-03-31 00:11:05 -0400

    added Table class

---
+++ wiki/type/Table.java
@@ -0,0 +1,115 @@
+package wiki.type;
+import java.util.*;
+
+public class Table {
+	private String[][] table;
+	private int rows, cols;
+	
+	/**
+	 * copies contents from input to table
+	 * @param input: 2D String array, must have known size, individual entries can be null
+	 */
+	public Table(String[][] input){
+		rows = input.length;
+		cols = input[0].length;
+		table = new String[rows][cols];
+		for (int i=0; i<rows; i++){
+			for (int j=0; j<cols; j++){
+				table[i][j]=input[i][j];
+			}
+		}	
+	}
+	
+	/**
+	 * set content of cell(x,y) to new entry
+	 * @param entry: a string you want to insert
+	 * @param x: row index
+	 * @param y: column index
+	 * @return true if insertion is successful, false o/w
+	 */
+	public boolean insert(String entry, int x, int y){
+		if (x>=0 && x<rows && y>=0 && y<cols){
+			table[x][y] = entry;
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * set content of cell(x,y) to empty string
+	 * @param x row index
+	 * @param y column index
+	 * @return true if removal is successful, false o/w
+	 */
+	public boolean remove(int x, int y){
+		if (x>=0 && x<rows && y>=0 && y<cols){
+			table[x][y] = "";
+			return true;
+		}
+		return false;
+	}
+	/**
+	 * return the content of cell(x,y)
+	 * @param x
+	 * @param y
+	 * @return null if coordinates are out of bounds, or entry is uninitialized.
+	 */
+	public String getEntry(int x, int y){
+		if (x>=0 && x<rows && y>=0 && y<cols){
+			return table[x][y];
+		}
+		else {return null;}
+	}
+
+	/**
+	 * return the size in form of String
+	 * @return e.g "3 rows 2 cols"
+	 */
+	public String getSize(){
+		return rows + " rows " + cols + " cols ";
+	}
+	
+	/**
+	 * search keyword in the table, case insensitive
+	 * @param keyword
+	 * @return a list of coordinates that contain keyword
+	 */
+	public List<String> search(String keyword){
+		List<String> results = new ArrayList<String>();
+		for (int i=0; i<rows; i++){
+			for (int j=0; j<cols; j++){
+				if (table[i][j].toLowerCase().contains(keyword.toLowerCase())){
+					results.add("("+i+","+j+")");
+				}
+			}
+		}
+		//System.out.println(results);
+		return results;
+	}
+	
+	/**
+	 * print to the console in the form:
+	 * row 0 xxx; xxx; ...
+	 * row 1 xxx; xxx; ...
+	 * ....
+	 */
+	public void print(){
+		int rows = table.length;
+		int cols = table[0].length;
+		System.out.println("\nTable");
+		for (int i=0; i<rows; i++){
+			System.out.print("row " +i+" ");
+			for (int j=0; j<cols; j++){
+				System.out.print(table[i][j]+";");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+	
+	//printToFile method?
+	
+	
+	
+	
+}
---
+++ wiki/type/Test.java
@@ -0,0 +1,88 @@
+package wiki.type;
+import java.util.*;
+import java.net.*;
+import java.io.*;
+
+//using Jsoup for now, will try regex when have time
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+
+public class Test {
+	public static void main(String[] args) {
+	
+	//testing basic Table class functions	
+	String[][] input = {{"a","b","c"},{"c","d","e"}};
+	Table table1= new Table(input);
+	table1.remove(0, 2);
+	table1.print();
+	table1.insert("C",0, 2);
+	table1.print();
+	System.out.println(table1.getSize());
+	System.out.println(table1.search("c"));
+	
+	//testing getTables method(see below)
+	String url = "http://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States";
+    ArrayList<Table> tables = getTables(url);
+    for (Table table:tables){
+    	table.print();
+    }
+	}
+    /**
+     * need import statement of Jsoup
+     * @param urlAddress: of type String
+     * @return an ArrayList of Table objects
+     */
+    public static ArrayList<Table> getTables (String urlAddress){
+    	 ArrayList<Table> tables = new ArrayList<Table>();
+    	 Document doc = null; 
+	     
+    	 //store html in doc
+    	 try {
+		    doc = Jsoup.connect(urlAddress).get();
+    	 }
+    	 catch (IOException e) {
+	    	e.printStackTrace();
+    	 }
+	     
+    	 //get only wikitables, not just any table
+		 Elements wikiTables = doc.getElementsByClass("wikitable");
+		 for (Element e : wikiTables){
+			Table table = null;
+			//first pass to get dimensions of table
+			int rows = 0;
+			int cols = 0;
+			for (Element row : e.select("tr")){
+				Elements tds = row.select("td");
+				if (tds.size() == 0) continue; //this row has no columns
+				cols = Math.max(cols, tds.size());
+				rows++;
+			}
+			
+			//construct new Table object 
+			if (rows==0) continue;
+			String[][] input = new String[rows][cols];
+			table = new Table(input);
+			
+			//second pass to store content in table
+			int i = 0;
+			for (Element row : e.select("tr")){
+				Elements tds = row.select("td");
+				if (tds.size() == 0) continue;
+				int j = 0;
+				for (Element td : tds){
+					table.insert(td.text(),i,j);
+					j++;
+				}
+				i++;	
+			}
+			
+			//add table to ArrayList
+			tables.add(table);
+		 }
+		 return tables;
+     }
+}
